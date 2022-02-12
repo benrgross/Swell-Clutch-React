8a5eca355af23865d22df503ba033e5a55e02703
Ben Gross <benrgross@gmail.com>
2022-02-12 15:55:44 -0500

    trying header config

--- routes/api/puppet.js
+++ routes/api/puppet.js
@@ -7,10 +7,12 @@ router.get("/getspots/:spot", async (req, res) => {
 
   try {
     let results = [];
-    const browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      headless: true,
-    });
+    const browser = await puppeteer.launch(
+      {
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      },
+      { headless: true }
+    );
     const page = await browser.newPage();
     await page.goto(`https://www.surfline.com/search/${req.params.spot}`);
 
