e374e981039473c4089ab5193cd5226a5b52a0c8
Ben Gross <benrgross@gmail.com>
2022-02-12 15:32:48 -0500

    cheerio puppet api call

--- routes/api/puppet.js
+++ routes/api/puppet.js
@@ -1,34 +1,49 @@
 const puppeteer = require("puppeteer");
 const router = require("express").Router();
+const cheerio = require("cheerio");
 
 router.get("/getspots/:spot", async (req, res) => {
   console.log(req.params);
 
   try {
+    let results = [];
     const browser = await puppeteer.launch({
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      headless: true,
     });
     const page = await browser.newPage();
-    await page.goto(
-      `https://www.surfline.com/search/${req.params.spot}`
-      // waitUntil: "networkidle0",
-    );
+    await page.goto(`https://www.surfline.com/search/${req.params.spot}`);
 
-    const text = await page.evaluate(() => {
-      // const name = Array.from(
-      //   document
-      //     .querySelector("#surf-spots")
-      //     .querySelectorAll(".SearchResults_result__5syZp"),
-      //   (element) => element.textContent
-      // );
+    const html = await page.content(); // serialized HTML of page DOM.
 
-      return document.querySelector(".Search_headline__rMElG").innerText;
+    const $ = cheerio.load(html);
+
+    $("#surf-spots > div > div").each((i, element) => {
+      let href = $(element).children("a").attr("href");
+      let spotId = href.split("/")[5];
+      let nameFromRef = href.split("/");
+      let name = nameFromRef[4].split("-").join(" ");
+
+      const spot = {
+        name: name,
+        spotId: spotId,
+        href: href,
+      };
+      results.push(spot);
     });
-    console.log("results", text);
-
     await browser.close();
+    res.status(200).json(results);
 
-    res.json(text);
+    // const text = await page.evaluate(() => {
+    //   // const name = Array.from(
+    //   //   document
+    //   //     .querySelector("#surf-spots")
+    //   //     .querySelectorAll(".SearchResults_result__5syZp"),
+    //   //   (element) => element.textContent
+    //   // );
+
+    //   return document.querySelector(".Search_headline__rMElG").innerText;
+    // });
   } catch (err) {
     res.send(err);
   }
