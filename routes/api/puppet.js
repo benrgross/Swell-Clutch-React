3be4d49d5ff7cd3980976e672f4d75a9266b3cd1
Ben Gross <benrgross@gmail.com>
2022-02-12 17:41:08 -0500

    set user agent test

--- routes/api/puppet.js
+++ routes/api/puppet.js
@@ -8,52 +8,54 @@ router.get("/getspots/:spot", async (req, res) => {
   try {
     let results = [];
     const browser = await puppeteer.launch({
-      headless: true,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
-        "--disable-infobars",
-        "--window-position=0,0",
-        "--ignore-certifcate-errors",
-        "--ignore-certifcate-errors-spki-list",
+        // "--disable-infobars",
+        // "--window-position=0,0",
+        // "--ignore-certifcate-errors",
+        // "--ignore-certifcate-errors-spki-list",
         '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
       ],
-      waitUntil: "networkidle0",
-      timeout: 0,
+      // waitUntil: "networkidle0",
+      // timeout: 0,
     });
     const page = await browser.newPage();
+    await page.setUserAgent(
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+    );
     await page.goto(`https://www.surfline.com/search/${req.params.spot}`);
 
-    const html = await page.content({ encoding: "base64" }); // serialized HTML of page DOM.
+    // await page.content(); // serialized HTML of page DOM.
+    const text = await page.evaluate(() => {
+      // const name = Array.from(
+      //   document
+      //     .querySelector("#surf-spots")
+      //     .querySelectorAll(".SearchResults_result__5syZp"),
+      //   (element) => element.textContent
+      // );
 
-    const $ = cheerio.load(html);
-
-    $("#surf-spots > div > div").each((i, element) => {
-      let href = $(element).children("a").attr("href");
-      let spotId = href.split("/")[5];
-      let nameFromRef = href.split("/");
-      let name = nameFromRef[4].split("-").join(" ");
-
-      const spot = {
-        name: name,
-        spotId: spotId,
-        href: href,
-      };
-      results.push(spot);
+      return document.querySelector(".Search_headline__rMElG").innerText;
     });
-    await browser.close();
-    res.status(200).json(results);
 
-    // const text = await page.evaluate(() => {
-    //   // const name = Array.from(
-    //   //   document
-    //   //     .querySelector("#surf-spots")
-    //   //     .querySelectorAll(".SearchResults_result__5syZp"),
-    //   //   (element) => element.textContent
-    //   // );
+    // const $ = cheerio.load(html);
 
-    //   return document.querySelector(".Search_headline__rMElG").innerText;
+    // $("#surf-spots > div > div").each((i, element) => {
+    //   let href = $(element).children("a").attr("href");
+    //   let spotId = href.split("/")[5];
+    //   let nameFromRef = href.split("/");
+    //   let name = nameFromRef[4].split("-").join(" ");
+
+    //   const spot = {
+    //     name: name,
+    //     spotId: spotId,
+    //     href: href,
+    //   };
+    //   console.log(spot);
+    //   results.push(spot);
     // });
+    await browser.close();
+    res.status(200).json(text);
   } catch (err) {
     res.send(err);
   }
