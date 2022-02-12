8feba272ee9d814fce27519988f81d46fe32e4e6
Ben Gross <benrgross@gmail.com>
2022-02-12 16:59:33 -0500

    new test

--- routes/api/puppet.js
+++ routes/api/puppet.js
@@ -9,7 +9,17 @@ router.get("/getspots/:spot", async (req, res) => {
     let results = [];
     const browser = await puppeteer.launch({
       headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-infobars",
+        "--window-position=0,0",
+        "--ignore-certifcate-errors",
+        "--ignore-certifcate-errors-spki-list",
+        '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
+      ],
+      waitUntil: "networkidle0",
+      timeout: 0,
     });
     const page = await browser.newPage();
     await page.goto(`https://www.surfline.com/search/${req.params.spot}`);
