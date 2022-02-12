0453115632214ae50d9737720df82ee20e7a39e0
Ben Gross <benrgross@gmail.com>
2022-02-12 18:41:14 -0500

    surfline

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const cheerioRoutes = require("./cheerio");
 const puppetRoutes = require("./puppet");
+const surflineRoutes = require("./surfline");
 
 router.use("/cheerio", cheerioRoutes);
 router.use("/puppet", puppetRoutes);
+router.use("/surfline", surflineRoutes);
 
 module.exports = router;
---
+++ routes/api/surfline.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const router = require("express").Router();
+
+router.post("/report ", async (req, res) => {
+  console.log("hello");
+  console.log(req.body);
+  try {
+    const { data } = await axios.get(
+      `http://services.surfline.com/kbyg/spots/forecasts/wave?spotId=${req.body.spotId}`
+    );
+    const { lat, lon } = data.associated.location;
+
+    const results = await axios.get(
+      `http://services.surfline.com/kbyg/mapview/spot?lat=${lat}&lon=${lon}`
+    );
+    res.status(200).json(results.data);
+  } catch (error) {
+    res.status(400).send(error);
+    console.log(error);
+  }
+});
+
+module.exports = router;
