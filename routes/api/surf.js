f3f6b502c8a145ba7745aad1f2d5208276d44119
Ben Gross <benrgross@gmail.com>
2022-02-16 09:13:25 -0500

    set user agent axios

--- routes/api/surf.js
+++ routes/api/surf.js
@@ -4,12 +4,25 @@ const axios = require("axios");
 router.post("/report", async (req, res) => {
   try {
     const { data } = await axios.get(
-      `http://services.surfline.com/kbyg/spots/forecasts/wave?spotId=${req.body.spotId}`
+      `http://services.surfline.com/kbyg/spots/forecasts/wave?spotId=${req.body.spotId}`,
+      {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+        },
+      }
     );
+
     const { lat, lon } = data.associated.location;
 
     const results = await axios.get(
-      `http://services.surfline.com/kbyg/mapview/spot?lat=${lat}&lon=${lon}`
+      `http://services.surfline.com/kbyg/mapview/spot?lat=${lat}&lon=${lon}`,
+      {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+        },
+      }
     );
     res.status(200).json(results.data);
   } catch (error) {
