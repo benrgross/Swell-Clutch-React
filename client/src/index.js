8241c3a0baf20ea288619c8266834196d898633c
Ben Gross <benrgross@gmail.com>
2022-02-21 20:33:22 -0500

    removed pupped from index

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./style.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
@@ -21,8 +20,3 @@ ReactDOM.render(
 
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
-const puppetRoutes = require("./puppet");
+
 const surflineRoutes = require("./surf");
 const dbRoutes = require("./db");
 
-router.use("/puppet", puppetRoutes);
 router.use("/surf", surflineRoutes);
 router.use("/session", dbRoutes);
 
